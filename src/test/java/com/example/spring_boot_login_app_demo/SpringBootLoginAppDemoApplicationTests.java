ea61ceec09086bae35ebb961467f6dc7a4b428a8
aki0000 <akihisa.fromm1@gmail.com>
2024-01-07 11:36:53 +0900

    init

---
+++ src/test/java/com/example/spring_boot_login_app_demo/SpringBootLoginAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_login_app_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoginAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
