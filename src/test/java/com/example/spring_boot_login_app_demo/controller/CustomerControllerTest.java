016d8b54efafce267474a749bfb60c43cdf0c21d
aki0000 <akihisa.fromm1@gmail.com>
2024-01-08 15:00:14 +0900

    fixed

--- src/test/java/com/example/spring_boot_login_app_demo/controller/CustomerControllerTest.java
+++ src/test/java/com/example/spring_boot_login_app_demo/controller/CustomerControllerTest.java
@@ -3,12 +3,7 @@ package com.example.spring_boot_login_app_demo.controller;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.hamcrest.Matchers.*;
-import static org.mockito.Mockito.doReturn;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.spring_boot_login_app_demo.security.SecurityConfig;
